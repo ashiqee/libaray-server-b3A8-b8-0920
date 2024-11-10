e925ff9c31e6a3136cb10321fabb65d00f8a230a
ashiqee <ashiq.buet73@gmail.com>
2024-11-10 21:54:35 +0600

    intial server setup and folder structure

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+
+
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send({
+        Message: "Liabary server are runing.."
+    })
+})
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { Server } from "http";
+import app from "./app";
+import config from "./config";
+
+
+async function main() {
+    const server: Server = app.listen(config.port,()=>{
+        console.log("Liabary Server running on port",config.port);
+        
+    })
+    
+};
+
+main()
